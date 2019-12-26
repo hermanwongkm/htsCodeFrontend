1799baf08af196fc361d6b5d4a53a0c9fb009a0e
Soong Cun Yuan <zengersoong@gmail.com>
2019-12-26 12:43:02 +0800

    Added search hy HTS code

--- src/api/query.js
+++ src/api/query.js
@@ -1,19 +1,38 @@
 import axios from "./index.js";
 
 export const getTable = async search => {
+  let regexp = /^(\d{4}|\d{3})(\.\d{2}(\.\d{2}(\.\d{2})?)?)?$/g  // This is to match the hts / codes
   search = search.toString().toLowerCase();
-  try {
-    const res = await axios.get(`/search/${search}`);
-    return {
-      extractedTable: res.data[0],
-      hit_list: res.data[1]
-    };
-  } catch (err) {
-    return {
-      extractedTable: [],
-      hit_list: []
-    };
-  }
+  // search by hts_code
+  if (search.match(regexp)!=null) {
+    try {
+      console.log(search);
+      const res = await axios.get(`/search/searchc/${search}`);
+      return {
+        extractedTable: res.data[0],
+        hit_list: res.data[1]
+      };
+    } catch (err) {
+      return {
+        extractedTable: [],
+        hit_list: []
+      };
+    }
+    
+  }else{ // search by keyword string
+    try {
+      const res = await axios.get(`/search/${search}`);
+      return {
+        extractedTable: res.data[0],
+        hit_list: res.data[1]
+      };
+    } catch (err) {
+      return {
+        extractedTable: [],
+        hit_list: []
+      };
+    }
+  };
 };
 
 export const updateDatabase = async () => {
---
+++ src/api/regex.js
@@ -0,0 +1,12 @@
+let str1 = "Leather bag";
+let str2 = '3210.00.00';
+let str3 = '302.84';
+
+
+const reSearch = (input) => {
+    let regexp1 = /^(\d{4}|\d{3})(\.\d{2}(\.\d{2}(\.\d{2})?)?)?$/g  // This is to match the hts / codes
+    let result1 = input.match(regexp1);
+    console.log(result1.toString().toLowerCase());
+};
+
+reSearch(str2);
